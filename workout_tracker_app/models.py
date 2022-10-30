fc982b31179f72dc000694349056c8a46b749e37
Matt <mjd014@live.co.uk>
2022-10-30 15:16:05 +0000

    created 3 models, workoutPost, Exercise and Comment models.

--- workout_tracker_app/models.py
+++ workout_tracker_app/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# Workout Post Model
+class WorkoutPost(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='workout_post'
+        )
+    featured_image = CloudinaryField('image', default='placeholder')
+    updated_on = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        User, related_name='workout_likes', blank=True
+        )
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+# Exercise Model
+class Exercise(models.Model):
+
+    workout = models.ForeignKey(
+        WorkoutPost, on_delete=models.CASCADE, related_name='exercise'
+        )
+    name = models.CharField(max_length=100)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    weight = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='created_by'
+        )
+
+    def __str__(self):
+        return self.name
+
+
+# Comment Model
+class Comment(models.Model):
+
+    workoutPost = models.ForeignKey(
+        WorkoutPost, on_delete=models.CASCADE, related_name='comments'
+        )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
