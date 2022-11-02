28d569ed256e8499851f33a4c5c234342b514b06
Matt <mjd014@live.co.uk>
2022-11-02 17:27:34 +0000

    removed 2 paths, added new one to exercise detail.

--- workout_tracker_app/urls.py
+++ workout_tracker_app/urls.py
@@ -5,10 +5,5 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.WorkoutList.as_view(), name='home'),
-    path(
-        'create/', login_required(
-            views.CreateExercise.as_view()
-            ), name='create_exercise'
-        ),
-    path('exerciselist/', views.ExerciseList.as_view(), name='exercise_list')
+    path('<slug:slug>', views.WorkoutDetail.as_view(), name='exercise_detail')
 ]
