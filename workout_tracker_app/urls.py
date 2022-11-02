7222ad308a3ed1d2d41d50cb1f3b9f1950f4c508
Matt <mjd014@live.co.uk>
2022-11-02 12:54:11 +0000

    added path to exercise_form and exercise_list

--- workout_tracker_app/urls.py
+++ workout_tracker_app/urls.py
@@ -1,7 +1,14 @@
 from . import views
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', views.WorkoutList.as_view(), name='home')
+    path('', views.WorkoutList.as_view(), name='home'),
+    path(
+        'create/', login_required(
+            views.CreateExercise.as_view()
+            ), name='create_exercise'
+        ),
+    path('exerciselist/', views.ExerciseList.as_view(), name='exercise_list')
 ]
