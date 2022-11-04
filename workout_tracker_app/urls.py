cc2545ac741ef5cff883e8726de587ce45fa625f
Matt <mjd014@live.co.uk>
2022-11-04 19:32:43 +0000

    created new path to add_exercise.

--- workout_tracker_app/urls.py
+++ workout_tracker_app/urls.py
@@ -6,4 +6,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', views.WorkoutList.as_view(), name='home'),
     path('<slug:slug>', views.WorkoutDetail.as_view(), name='workout_detail'),
+    path(
+        '<slug:slug>/add', views.AddExerciseForm.as_view(), name='add_exercise'
+        ),
 ]
