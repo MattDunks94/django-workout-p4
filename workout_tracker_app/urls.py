a22b630093733f962e6af9123c0ed8ac95129c5d
Matt <mjd014@live.co.uk>
2022-11-05 15:20:25 +0000

    removed path to add_exercise as html file is removed.

--- workout_tracker_app/urls.py
+++ workout_tracker_app/urls.py
@@ -6,7 +6,4 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', views.WorkoutList.as_view(), name='home'),
     path('<slug:slug>', views.WorkoutDetail.as_view(), name='workout_detail'),
-    path(
-        '<slug:slug>/add', views.AddExerciseForm.as_view(), name='add_exercise'
-        ),
 ]
